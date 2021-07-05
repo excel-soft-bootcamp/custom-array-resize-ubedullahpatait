274eda5089ee43be0142ea2a370dc827657c0d4e
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-07-05 16:43:07 +0530

    Add files via upload

---
+++ Array_Utility/ArrayUtility/ArrayUtility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayUtility
+{
+    public static class ArrayUtility
+    {
+
+        public static int Resize(ref int[] array, int newSize)
+        {
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine(array[i]);
+				//newSize++;
+
+			}
+			array = new int[newSize];
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine(array[i]);
+				//newSize++;
+
+			}
+			Console.WriteLine((array));
+			// arr[5] = 'n';
+			return array[5];
+
+
+		
+      
+
+
+		}
+
+	}
+}
